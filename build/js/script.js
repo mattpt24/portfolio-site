7a2183cc9495b3e514fec2fed662a407e22be3b0
Matt Persell-Thompson <danielpt@live.co.uk>
2023-09-07 22:39:49 +0100

    feat: Added Sidebar Menu

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,17 @@
 console.log("If you see this, JS is working");
+
+
+const menuWrapper = document.querySelector(".menu__wrapper");
+const menuContainer = document.querySelector(".menu__container");
+const menuToggleBtn = document.querySelector(".nav__toggle-menu");
+
+menuToggleBtn.addEventListener("click", () => {
+    menuWrapper.classList.add("menu--activated");
+    menuContainer.classList.add("menu-content--activated");
+});
+
+
+menuWrapper.addEventListener("click", () => {
+    menuWrapper.classList.remove("menu--activated");
+    menuContainer.classList.remove("menu-content--activated");
+});
--- dist/js/all.js
+++ dist/js/all.js
@@ -1,3 +1,14 @@
 "use strict";
 
 console.log("If you see this, JS is working");
+var menuWrapper = document.querySelector(".menu__wrapper");
+var menuContainer = document.querySelector(".menu__container");
+var menuToggleBtn = document.querySelector(".nav__toggle-menu");
+menuToggleBtn.addEventListener("click", function () {
+  menuWrapper.classList.add("menu--activated");
+  menuContainer.classList.add("menu-content--activated");
+});
+menuWrapper.addEventListener("click", function () {
+  menuWrapper.classList.remove("menu--activated");
+  menuContainer.classList.remove("menu-content--activated");
+});
